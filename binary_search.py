6e223c9ae74a271927d089445259b1fd05474d71
Denis Serdyuk <diserdyuk@gmail.com>
2022-11-04 17:37:43 +0100

    Test

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+def binar_serch(list, nums):
+    low = 0
+    high = len(list) - 1
+
+    while low <= high:
+        mid = low + high
+        guess = list[mid]
+
+        if guess == nums:
+            return mid
+        if guess > nums:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+
+list_1 = [1, 2, 3, 4, 5, 6, 7]
+
+print(binar_serch(list_1, 4))
+
+# ReAD
+# http://aliev.me/runestone/SortSearch/TheBinarySearch.html
